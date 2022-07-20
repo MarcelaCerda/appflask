dabdf4d9d032bfdbc34c2f34d355033688f6cd55
marcela <marcela.ines.cerda@gmail.com>
2022-07-20 15:43:59 -0300

    sss

--- app.py
+++ app.py
@@ -161,6 +161,4 @@ def update_usuario(id):
 
 # programa principal ****************************************
 if __name__=='__main__':  
-    app.run( debug=True )
-
-#debug=True, port=5000
+    app.run( debug=True, port=5000)
